b9fa81b7b778c95557094389a06c097044e5af7e
Christopher Arndt <chris@chrisarndt.de>
2014-04-16 13:48:38 +0200

    Fix two Python 3 incompatibilities (fixes #3)

--- flask_zodb.py
+++ flask_zodb.py
@@ -2,7 +2,11 @@ import flask
 import transaction
 import zodburi
 
-from UserDict import IterableUserDict
+try:
+    from collections import UserDict
+except ImportError: # for Python 2
+    from UserDict import IterableUserDict as UserDict
+
 from ZODB.DB import DB
 from contextlib import contextmanager, closing
 from werkzeug.utils import cached_property
@@ -12,11 +16,17 @@ from persistent import Persistent as Object
 from persistent.list import PersistentList as List
 from persistent.mapping import PersistentMapping as Dict
 
+# Python 3 compatibility
+try:
+    basestring
+except NameError:
+    basestring = str
+
 
 __all__ = ['ZODB', 'Object', 'List', 'Dict', 'BTree']
 
 
-class ZODB(IterableUserDict):
+class ZODB(UserDict):
     """Extension object.  Behaves as the root object of the storage during
     requests, i.e. a `~persistent.mapping.PersistentMapping`.
 
